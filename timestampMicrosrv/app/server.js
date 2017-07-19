620be79fd2f9ae914b59660e3bdb35bebf342729
thefalconx33 <thefalconx33@gmail.com>
2017-07-19 13:57:18 +0530

    Add files via upload

---
+++ timestampMicrosrv/app/server.js
@@ -0,0 +1,55 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+var moment = require('moment');
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/dreams", function (request, response) {
+  response.send(dreams);
+});
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", function (request, response) {
+  dreams.push(request.query.dream);
+  response.sendStatus(200);
+});
+
+app.get('/:string' , function (req, res) { 
+  var string = moment(req.params.string);
+  if(string._isValid){
+      res.send({ 
+        unix: parseInt(moment(string).format("X")),
+        natural: moment(string).format("MMMM D, YYYY") 
+      });
+    }
+    else{
+      res.send({
+        unix: null,
+        natural: null
+      });
+    };
+})
+// Simple in-memory store for now
+var dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
