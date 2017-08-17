85cfc2b38e2480186795bb3b8ec658fdfb27e716
thefalconx33 <thefalconx33@gmail.com>
2017-08-17 20:14:12 +0530

    fileMetadata Microservice

---
+++ fileMetadata/app/public/client.js
@@ -0,0 +1,27 @@
+// client-side js
+// run by the browser each time your view template is loaded
+
+// by default, you've got jQuery,
+// add other scripts at the bottom of index.html
+$('document').ready(function(){
+//   var file;
+//   $('#upfile').on('change', function(){
+//     file = this.files[0];      
+//   });
+  
+//   $('#upload').on('click', function(){
+//     var data = new FormData(file);
+//     console.log(file);
+//     $.ajax({
+//       url: '/upload',
+//       data: file,
+//       cache: false,
+//       contentType: false,
+//       processData: false,
+//       type: 'POST',
+//       success: function(data){
+//         alert(data);
+//       }
+//     });
+//   })
+})
---
+++ fileMetadata/app/server.js
@@ -0,0 +1,32 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/upload', upload.single('file'), function (req, res, next) {
+  res.json({
+   size: req.file.size + "B",
+   size_kb: Math.floor(req.file.size/1024) + "KB"
+  });
+})
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
